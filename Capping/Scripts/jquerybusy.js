83cce2ee843beb73ce99ec67753febaaed904666
bpatton00 <bpatton00@yahoo.com>
2016-12-02 09:06:46 -0600

    Add files via upload

---
+++ Capping/Scripts/jquerybusy.js
@@ -0,0 +1,26 @@
+﻿// prepare the form when the DOM is ready 
+$(document).ready(function () {
+
+    // Setup the ajax indicator
+    $('body').append('<div id="ajaxBusy"><p><img src="/images/loading.gif"></p></div>');
+
+    $('#ajaxBusy').css({
+        display: "none",
+        margin: "0px",
+        paddingLeft: "0px",
+        paddingRight: "0px",
+        paddingTop: "0px",
+        paddingBottom: "0px",
+        position: "absolute",
+        right: "0px",
+        top: "0px",
+        width: "auto"
+    });
+});
+
+// Ajax activity indicator bound to ajax start/stop document events
+$(document).ajaxStart(function () {
+    $('#ajaxBusy').show();
+}).ajaxStop(function () {
+    $('#ajaxBusy').fadeOut("slow");
+});
